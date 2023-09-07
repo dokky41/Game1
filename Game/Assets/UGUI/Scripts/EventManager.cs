a18e0bee746c3fcd2129ff4144504e8d8447c51b
Dokky <107966608+dokky41@users.noreply.github.com>
2023-09-07 21:44:43 +0900

    (2023.9.7) UI 간단하게 다뤄보기

---
+++ Game/Assets/UGUI/Scripts/EventManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventManager : MonoBehaviour
+{
+    
+    public void Message()
+    {
+        Debug.Log("Event ¹ß»ý~");
+    }
+
+
+
+
+
+}
