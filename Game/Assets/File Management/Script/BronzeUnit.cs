3d78ac1009034c8db760fb0ea77223167c7373bd
Dokky <107966608+dokky41@users.noreply.github.com>
2023-09-15 21:48:10 +0900

    (2023.9.15) CSV 파일 관리

---
+++ Game/Assets/File Management/Script/BronzeUnit.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BronzeUnit : Unit
+{
+    protected override void Movement()
+    {
+        Debug.Log("¿Ãµø");
+    }
+
+    public override void Initialize(string fileName,int count)
+    {
+        data = CSVReader.Read(fileName);
+
+        name = (string)data[count]["name"];
+        health = System.Convert.ToInt32(data[count]["health"]);
+        attack = System.Convert.ToInt32(data[count]["attack"]);
+    }
+
+}
